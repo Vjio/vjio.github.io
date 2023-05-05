8f2508f8b77ba6efbcd1d84e5183bc5cf1056a65
DeadFishEyes2 <122477630+DeadFishEyes2@users.noreply.github.com>
2023-05-06 02:19:29 +0300

    STOCK MARKET
    Stock Market-ul este functional, poti selecta actiunile de la ce companie sa-ti fie afisate si poti cumpara si vinde actiuni individuale la oricare companie. In commit-ul asta nu se salveaza nr de stock-uri, nu sunt piete blocate si actiunile sunt cam aceasi chestie, trebuie customizate

--- minigame2.js
+++ minigame2.js
@@ -4,8 +4,8 @@ const canvas2 = document.getElementById("canvas2");
 		const Balance = document.getElementById("points");
 		
 		//let money = 1000;
-		let curentPrice = 150;
-		let nrStocks = 0;
+		let curentPrice = Array.from({length: 10}, () => 0);
+		let nrStocks = Array.from({length: 10}, () => 0);
 
 		//buy/sell
 		const buyButton = document.getElementById("buy");
@@ -16,24 +16,49 @@ const canvas2 = document.getElementById("canvas2");
 			document.getElementById("stock-market-button").style.display = "none";
 			document.getElementById("minigame2").style.display = "grid";
 
+			let curentStock = 0;
+
+		//setting the buy and sell buttons 
+
+
 		buyButton.addEventListener("click", () => {
-			if (numPoints > curentPrice){
-				numPoints -= curentPrice;
-				nrStocks++;
-				document.getElementById("number-of-stocks").innerHTML = nrStocks;
+			if (numPoints > curentPrice[curentStock]){
+				numPoints -= curentPrice[curentStock];
+				nrStocks[curentStock]++;
+				document.getElementById("number-of-stocks").innerHTML = nrStocks[curentStock];
 				Balance.innerHTML = numPoints;
 			}
 		});
 
 		sellButton.addEventListener("click", () => {
-			if (nrStocks > 0){
-				nrStocks--;
-				numPoints += curentPrice;
-				document.getElementById("number-of-stocks").innerHTML = nrStocks;
+			if (nrStocks[curentStock] > 0){
+				nrStocks[curentStock]--;
+				numPoints += curentPrice[curentStock];
+				document.getElementById("number-of-stocks").innerHTML = nrStocks[curentStock];
 				Balance.innerHTML = numPoints;
 			}
 		});
 
+
+		document.getElementById("prev-stock").addEventListener("click", ()=>{
+			if (curentStock == 0){
+				curentStock = 9;
+			} else { 
+				curentStock--;
+			}
+			document.getElementById("number-of-stocks").innerHTML = nrStocks[curentStock];
+		});
+
+		document.getElementById("next-stock").addEventListener("click", ()=>{
+			if (curentStock == 9){
+				curentStock = 0;
+			} else { 
+				curentStock++;
+			}
+			document.getElementById("number-of-stocks").innerHTML = nrStocks[curentStock];
+		});
+
+		
 		// Set the canvas size
 		canvas2.width = 400;
 		canvas2.height = 300;
@@ -43,48 +68,72 @@ const canvas2 = document.getElementById("canvas2");
 				const numStocks = 30; // Number of stocks to simulate
 
 			// initiating the stock
+
 				let stockPrices = [];
-				let stockTimes = [0];
+				for (let i = 0; i < 10; i++){
+					stockPrices[i] = [];
+				};
+
+				stockPrices[0][0] = 150;
+				stockPrices[1][0] = 150;
+				stockPrices[2][0] = 150;
+				stockPrices[3][0] = 150;
+				stockPrices[4][0] = 150;
+				stockPrices[5][0] = 150;
+				stockPrices[6][0] = 150;
+				stockPrices[7][0] = 150;
+				stockPrices[8][0] = 150;
+				stockPrices[9][0] = 150;
+
+				let stockTimes = [];
+				for (let i = 0; i < 40; i++){
+					stockTimes[i] = [];
+				};
+
+				for (let i = 0; i < 10; i++){
+					stockTimes[i][0] = 0;
+				}
+
 				let newStockPrice;
 				let volatility = 4; //less is more
 				let luck = 0.5; // more is more
 
-				stockPrices[0] = 150;
-
-				for (let i = 1; i < numStocks; i++) {
-					stockTimes.push(stockTimes[i-1] + Math.floor(Math.random() * 5)*10 + 15);
-					if (Math.random() < luck){
-						newStockPrice = (stockPrices[i-1] - (stockPrices[i-1] * Math.random()/volatility)) % 300;
-				 	} else {
-						newStockPrice = (stockPrices[i-1] + (stockPrices[i-1] * Math.random()/volatility)) % 300;
+				for (let index = 0; index < 10; index++){
+					for (let i = 1; i < numStocks; i++) {
+						stockTimes[index].push(stockTimes[index][i-1] + Math.floor(Math.random() * 5)*10 + 15);
+						if (Math.random() < luck){
+							newStockPrice = (stockPrices[index][i-1] - (stockPrices[index][i-1] * Math.random()/volatility)) % 300;
+				 		} else {
+							newStockPrice = (stockPrices[index][i-1] + (stockPrices[index][i-1] * Math.random()/volatility)) % 300;
+						}
+						if (newStockPrice < 30)
+							newStockPrice = 50;
+						stockPrices[index].push(newStockPrice);
 					}
-					if (newStockPrice < 30)
-						newStockPrice = 50;
-					stockPrices.push(newStockPrice);
 				}
 
 		//drawing the graph
 		const getStockPrice = () => {
 			let lastStock = 0;
-			while (stockTimes[lastStock+1] <= canvas2.width){
+			while (stockTimes[curentStock][lastStock+1] <= canvas2.width){
 				lastStock++;
 			}
 			
-				let X = stockTimes[lastStock+1] - stockTimes[lastStock];
+				let X = stockTimes[curentStock][lastStock+1] - stockTimes[curentStock][lastStock];
 			
 			//console.log(y);
-			if (stockPrices[lastStock+1] > stockPrices[lastStock]){
-				let x = stockTimes[lastStock+1] - canvas2.width;
-				let Y = stockPrices[lastStock+1] - stockPrices[lastStock];
+			if (stockPrices[curentStock][lastStock+1] > stockPrices[curentStock][lastStock]){
+				let x = stockTimes[curentStock][lastStock+1] - canvas2.width;
+				let Y = stockPrices[curentStock][lastStock+1] - stockPrices[curentStock][lastStock];
 				let y = (Y*(X-x))/X;
-				curentPrice = Math.floor(300 - (stockPrices[lastStock] + y));
+				curentPrice[curentStock] = Math.floor(300 - (stockPrices[curentStock][lastStock] + y));
 			} else { 
-				let x = stockTimes[lastStock+1] - canvas2.width;
-				let Y = stockPrices[lastStock+1] - stockPrices[lastStock];
+				let x = stockTimes[curentStock][lastStock+1] - canvas2.width;
+				let Y = stockPrices[curentStock][lastStock+1] - stockPrices[curentStock][lastStock];
 				let y = (Y*x)/X;
-				curentPrice = Math.floor(300 - (stockPrices[lastStock+1] - y));
+				curentPrice[curentStock] = Math.floor(300 - (stockPrices[curentStock][lastStock+1] - y));
 			}
-			stockPrice.innerHTML = curentPrice;
+			stockPrice.innerHTML = curentPrice[curentStock];
 			//console.log(lastStock);
 		};
 
@@ -98,14 +147,14 @@ const canvas2 = document.getElementById("canvas2");
 			for(let index = 0; index < numStocks; index++){
 				if(index == 0){
 					ctx2.strokeStyle = "yellow";
-					ctx2.moveTo(stockTimes[index],stockPrices[index]);
+					ctx2.moveTo(stockTimes[curentStock][index],stockPrices[curentStock][index]);
 				} else {
-					ctx2.lineTo(stockTimes[index],stockPrices[index]);
+					ctx2.lineTo(stockTimes[curentStock][index],stockPrices[curentStock][index]);
 				}
 			};
 			ctx2.stroke();
 			for (let index = 0; index < numStocks; index++){
-				stockTimes[index] -= 1;
+				stockTimes[curentStock][index] -= 1;
 			}
 
 			//displaying scale on the graph
@@ -120,18 +169,18 @@ const canvas2 = document.getElementById("canvas2");
 			ctx2.fillText("100",350,200);
 			ctx2.fillText("50",350,250);
 			//setting a new price for the stock
-			if (stockTimes[1] < 0){
-				stockTimes.shift();
-				stockPrices.shift();
+			if (stockTimes[curentStock][1] < 0){
+				stockTimes[curentStock].shift();
+				stockPrices[curentStock].shift();
 				if (Math.random() < luck){
-						newStockPrice = (stockPrices[numStocks-2] - (stockPrices[numStocks-2] * Math.random()/volatility) + 20) % 290;
+						newStockPrice = (stockPrices[curentStock][numStocks-2] - (stockPrices[curentStock][numStocks-2] * Math.random()/volatility) + 20) % 290;
 				} else {
-						newStockPrice = (stockPrices[numStocks-2] + (stockPrices[numStocks-2] * Math.random()/volatility) + 20) % 290;
+						newStockPrice = (stockPrices[curentStock][numStocks-2] + (stockPrices[curentStock][numStocks-2] * Math.random()/volatility) + 20) % 290;
 				}
 				if (newStockPrice < 30)
 					newStockPrice = 50;
-				stockTimes.push(stockTimes[numStocks-2] + Math.floor(Math.random() * 5)*10 + 10);
-				stockPrices.push(newStockPrice);
+				stockTimes[curentStock].push(stockTimes[curentStock][numStocks-2] + Math.floor(Math.random() * 5)*10 + 10);
+				stockPrices[curentStock].push(newStockPrice);
 			}
 			getStockPrice();
 		};
