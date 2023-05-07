03ef9743b429c65445a0d07c3ca84bc0449bdea5
DasinuV0 <132465059+DasinuV0@users.noreply.github.com>
2023-05-07 17:13:43 +0300

    Update saving.js

--- saving.js
+++ saving.js
@@ -1,6 +1,8 @@
 function saveData(){
     localStorage.setItem("numPoints", numPoints);
     localStorage.setItem("buildingLevel", JSON.stringify(buildingLevel));
+    localStorage.setItem("buildingCost", JSON.stringify(buildingCost));
+    localStorage.setItem("buildingIncome", JSON.stringify(buildingIncome));
     localStorage.setItem("fighterLevel", JSON.stringify(fighterLevel));
     localStorage.setItem("upgradeBool", JSON.stringify(upgradeBool));
     localStorage.setItem("numberOfStocks", JSON.stringify(nrStocks));
@@ -8,17 +10,17 @@ function saveData(){
 
 function loadGame(){ //a fost..... dificil sa scriu asta
     numPoints = parseFloat(localStorage.getItem("numPoints")) || 0; // stochez numarul de puncte aici
-    buildingLevel = JSON.parse(localStorage.getItem("buildingLevel")) || Array(10).fill(0); // sunt un geniu si am transformat datele butoanelor in array-uri, astfel codul poate fi scris in loopuri ca sa-mi bat capul o singura data cu butoanele
-    fighterLevel = JSON.parse(localStorage.getItem("fighterLevel")) || Array(10).fill(0);
-    upgradeBool = JSON.parse(localStorage.getItem("upgradeBool")) || Array(10).fill(false);
-    nrStocks = JSON.parse(localStorage.getItem("numberOfStocks")) || Array(10).fill(0);
+    buildingLevel = JSON.parse(localStorage.getItem("buildingLevel")) || Array(12).fill(0); // sunt un geniu si am transformat datele butoanelor in array-uri, astfel codul poate fi scris in loopuri ca sa-mi bat capul o singura data cu butoanele
+    buildingCost = JSON.parse(localStorage.getItem("buildingCost")) || Array(12).fill(0);
+    buildingIncome = JSON.parse(localStorage.getItem("buildingIncome")) || Array(12).fill(0);
+    fighterLevel = JSON.parse(localStorage.getItem("fighterLevel")) || Array(12).fill(0);
+    upgradeBool = JSON.parse(localStorage.getItem("upgradeBool")) || Array(12).fill(false);
+    nrStocks = JSON.parse(localStorage.getItem("numberOfStocks")) || Array(12).fill(0);
     document.getElementById("points").innerHTML = convert(numPoints);
-    for (let i = 1; i <= 8; i++){
+    for (let i = 1; i <= 11; i++) {
         document.getElementById("building" + i + "-level").innerHTML = buildingLevel[i];
         document.getElementById("progress-bar-" + i).addEventListener("animationiteration", () =>{animationUpdate (i)});
-        document.getElementById("building" + i + "-cost").innerHTML = convert(buildingCost[i]*(Math.pow(1.15,buildingLevel[i])));
-        buildingTime[i] = i;
-        buildingTime[i] = buildingTime[i]/(Math.pow(2, buildingLevel[i]/25));
+        document.getElementById("building" + i + "-cost").innerHTML = convert(buildingCost[i]);
     }
     hideElements();
     displayUpgrade();
