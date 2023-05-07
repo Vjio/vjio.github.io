2498f122cc2a9076f981ef5821bf991572f02655
DasinuV0 <132465059+DasinuV0@users.noreply.github.com>
2023-05-07 18:47:59 +0300

    Update saving.js

--- saving.js
+++ saving.js
@@ -6,13 +6,15 @@ function saveData(){
     localStorage.setItem("fighterLevel", JSON.stringify(fighterLevel));
     localStorage.setItem("upgradeBool", JSON.stringify(upgradeBool));
     localStorage.setItem("numberOfStocks", JSON.stringify(nrStocks));
+    localStorage.setItem("buildingTime", JSON.stringify(buildingTime));
 }
 
 function loadGame(){ //a fost..... dificil sa scriu asta
     numPoints = parseFloat(localStorage.getItem("numPoints")) || 0; // stochez numarul de puncte aici
     buildingLevel = JSON.parse(localStorage.getItem("buildingLevel")) || Array(12).fill(0); // sunt un geniu si am transformat datele butoanelor in array-uri, astfel codul poate fi scris in loopuri ca sa-mi bat capul o singura data cu butoanele
-    buildingCost = JSON.parse(localStorage.getItem("buildingCost")) || Array(12).fill(0);
+    buildingCost = JSON.parse(localStorage.getItem("buildingCost")) || [1,7,11,115,257,780,1567,4123,9134,23456,61345];
     buildingIncome = JSON.parse(localStorage.getItem("buildingIncome")) || Array(12).fill(0);
+    buildingTime = JSON.parse(localStorage.getItem("buildingTime")) || [2,4,8,16,32,64,128,256,512,1024,2048];
     fighterLevel = JSON.parse(localStorage.getItem("fighterLevel")) || Array(12).fill(0);
     upgradeBool = JSON.parse(localStorage.getItem("upgradeBool")) || Array(12).fill(false);
     nrStocks = JSON.parse(localStorage.getItem("numberOfStocks")) || Array(12).fill(0);
@@ -22,6 +24,7 @@ function loadGame(){ //a fost..... dificil sa scriu asta
         document.getElementById("progress-bar-" + i).addEventListener("animationiteration", () =>{animationUpdate (i)});
         document.getElementById("building" + i + "-cost").innerHTML = convert(buildingCost[i]);
     }
+    display();
     hideElements();
     displayUpgrade();
     displayProgressBar();
